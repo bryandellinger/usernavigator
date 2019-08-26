7552d9896e49eedc93db5a48dd8bc7b97c0d74af
bryandellinger <dellingerfam5@gmail.com>
2019-08-26 07:18:38 -0400

    Add files via upload

---
+++ UserNavigator/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using UserNavigator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UserNavigator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();     
+    }
+}
---
+++ UserNavigator/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserNavigator.Models;
+
+namespace UserNavigator.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+
+        private IEmployeesRepository repository;
+
+        public UsersController(IEmployeesRepository repo) => repository = repo;
+
+        [Authorize]
+        [HttpGet("{search}")]
+        public IActionResult Get(string search) => Ok(repository.Get(search == "null" ? null : search));
+    }
+}
