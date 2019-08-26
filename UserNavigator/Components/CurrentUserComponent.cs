0fd60a2e1f796fb70ce0e66804cb656d36f5efcc
bryandellinger <dellingerfam5@gmail.com>
2019-08-26 07:17:33 -0400

    Add files via upload

---
+++ UserNavigator/Components/CurrentUserComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserNavigator.Models;
+
+namespace UserNavigator.Components
+{
+    public class CurrentUserComponent : ViewComponent
+    {
+        private ICurrentUserRepository repository;
+
+        public CurrentUserComponent(ICurrentUserRepository repo) => repository = repo;
+
+        public IViewComponentResult Invoke() => View(repository.Get(User.Identity.Name, HttpContext));
+
+    }
+}
