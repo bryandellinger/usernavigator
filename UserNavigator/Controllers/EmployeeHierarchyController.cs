f61a078fd8ea3c85252c30759803bb3b6ff039e1
bryandellinger <dellingerfam5@gmail.com>
2019-08-28 07:54:06 -0400

    Add files via upload

---
+++ UserNavigator/Controllers/EmployeeHierarchyController.cs
@@ -0,0 +1,25 @@
+﻿
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UserNavigator.Models;
+using System.Linq;
+
+namespace UserNavigator.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/EmployeeHierarchy")]
+    public class EmployeeHierarchyController : Controller
+    {
+        private IEmployeeHierarchyRepository repository;
+
+        public EmployeeHierarchyController(IEmployeeHierarchyRepository repo) => repository = repo;
+
+        [HttpGet("{posNo}")]
+        public IActionResult Get(string posNo)
+        {
+            var result = repository.Get(posNo).ToArray();
+            return Ok(result);
+        }
+    }
+}
